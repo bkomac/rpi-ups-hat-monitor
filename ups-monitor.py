8a5cfd17308c3db1bd9f1b68c187c5966563d738
Borut Komac <borut@komac.si>
2021-10-08 16:58:06 +0200

    cc

--- ups-monitor.py
+++ ups-monitor.py
@@ -1,60 +1,86 @@
 import struct
 import smbus
 import sys
+import time
+import paho.mqtt.client as mqtt
+
 
 def readVoltage(bus):
-
-	"This function returns as float the voltage from the Raspi UPS Hat via the provided SMBus object"
-	address = 0x36
-	read = bus.read_word_data(address, 2)
-	swapped = struct.unpack("<H", struct.pack(">H", read))[0]
-	voltage = swapped * 78.125 /1000000
-	return voltage
+    "This function returns as float the voltage from the Raspi UPS Hat via the provided SMBus object"
+    address = 0x36
+    read = bus.read_word_data(address, 2)
+    swapped = struct.unpack("<H", struct.pack(">H", read))[0]
+    voltage = swapped * 78.125 / 1000000
+    return voltage
 
 
 def readCapacity(bus):
-	"This function returns as a float the remaining capacity of the battery connected to the Raspi UPS Hat via the provided SMBus object"
-	address = 0x36
-	read = bus.read_word_data(address, 4)
-	swapped = struct.unpack("<H", struct.pack(">H", read))[0]
-	capacity = swapped/256
-	return capacity
+    "This function returns as a float the remaining capacity of the battery connected to the Raspi UPS Hat via the provided SMBus object"
+    address = 0x36
+    read = bus.read_word_data(address, 4)
+    swapped = struct.unpack("<H", struct.pack(">H", read))[0]
+    capacity = swapped/256
+    return capacity
 
+
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code "+str(rc))
+    client.subscribe("sensor/iopi/ports")
+
+
+def on_message(client, userdata, msg):
+    message = str(msg.payload)
+    print(msg.topic+" "+message)
+
+
+def on_publish(mosq, obj, mid):
+    print("mid: " + str(mid))
+
+
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_message = on_message
+client.connect("node.komac.si", 11883, 60)
+client.loop_start()
 
 bus = smbus.SMBus(1)  # 0 = /dev/i2c-0 (port I2C0), 1 = /dev/i2c-1 (port I2C1)
 
-print "Voltage:%5.2fV" % readVoltage(bus)
+print ("Voltage:%5.2fV" % readVoltage(bus))
 
-print "Battery:%5i%%" % readCapacity(bus)
-
-
+print ("Battery:%5i%%" % readCapacity(bus))
 
 
 # draw battery
 
-n = int(round(readCapacity(bus) / 10));
+n = int(round(readCapacity(bus) / 10))
 
-print "----------- "
+print ("----------- ")
 
 sys.stdout.write('|')
 
-for i in range(0,n):
+for i in range(0, n):
 
-	sys.stdout.write('#')
+    sys.stdout.write('#')
 
-for i in range(0,10-n):
+for i in range(0, 10-n):
 
-	sys.stdout.write(' ')
+    sys.stdout.write(' ')
 
 sys.stdout.write('|+\n')
 
-print "----------- "
+print ("----------- ")
 
 
 if readCapacity(bus) == 100:
 
-	print "Battery FULL"
+    print ("Battery FULL")
 
 if readCapacity(bus) < 20:
 
-	print "Battery LOW"
+    print ("Battery LOW")
+
+
+while True:
+  sensor_data = [readVoltage(bus), readVoltage(bus)]
+  client.publish("pi05/ups", str(sensor_data))
+  time.sleep(10)
